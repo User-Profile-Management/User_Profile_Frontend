730ad6f357731ebc59cdb8965afc1f4d78d27ebd
ajusajeev111 <ajusajeev.111@gmail.com>
2025-03-24 18:00:05 +0530

    admin dashboard completed

---
+++ src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import DashboardLayout from '../../layout/DashboardLayout'
+import StatCard from '../../components/StatCard'
+import AddButton from "../../assets/add-button.svg"
+import Profile from "../../assets/profile-ListCard.svg"
+import ListCard from '../../components/ListCard'
+
+function AdminDashboard() {
+
+    const students = [
+        { name: 'John Doe', subtitle: 'Admission No: 12345', image: Profile },
+        { name: 'Jane Smith', subtitle: 'Admission No: 67890', image: Profile },
+        { name: 'John Doe', subtitle: 'Admission No: 12345', image: Profile },
+        { name: 'Jane Smith', subtitle: 'Admission No: 67890', image: Profile },
+        { name: 'John Doe', subtitle: 'Admission No: 12345', image: Profile },
+        { name: 'Jane Smith', subtitle: 'Admission No: 67890', image: Profile },
+        { name: 'John Doe', subtitle: 'Admission No: 12345', image: Profile },
+        { name: 'Jane Smith', subtitle: 'Admission No: 67890', image: Profile }
+      ];
+
+    return (
+
+        <DashboardLayout>
+            <div className='grid grid-rows-10 h-full'>
+                <div>
+                    <div className="text-2xl font-semibold">Home</div>
+                    <div className='text-zinc-500'>Admin Dashboard</div>
+                </div>
+                <div className="row-span-9">
+                    <div className="grid grid-cols-3 gap-6 h-full">
+
+                        <div className='grid grid-rows-5 gap-6 '>
+                            <StatCard number="37" title="Total Students" color="bg-cyan-600" />
+                            <ListCard title="Student List" items={students} showDelete={false}/>
+                        </div>
+
+                        <div className='grid grid-rows-5 gap-6 '>
+                            <StatCard number="8" title="Total Mentors" color="bg-cyan-700" />
+                            <ListCard title="Mentor List" items={students} showDelete={false}/>
+                        </div>
+                        <div className='grid grid-rows-5 gap-6 '>
+                            <StatCard number="20" title="Total Projects" color="bg-cyan-800" />
+                            <ListCard title="Project List" items={students} showDelete={true}/>
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </DashboardLayout>
+      )
+    
+}
+
+export default AdminDashboard
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,7 @@ import { Routes,Route } from 'react-router-dom'
 import SignIn from '../pages/SignIn'
 import SignUp1 from '../pages/SignUp1'
 import SignUp2 from '../pages/SignUp2'
+import AdminDashboard from '../pages/admin/AdminDashboard'
 
 function Router() {
   return (
@@ -12,6 +13,8 @@ function Router() {
        <Route path="/" element= {<SignIn/>} />
        <Route path="/signup" element= {<SignUp1/>} />
        <Route path="/signup2" element= {<SignUp2/>} />
+       
+       <Route path="/admin-dashboard" element={<AdminDashboard />} />
    </Routes>
   );
 }
