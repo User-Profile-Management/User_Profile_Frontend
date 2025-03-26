49549c4bbcb0f316dfd15df1ee33193a4dbc914b
v-sreevidya <sreevidyavijayan14@gmail.com>
2025-03-26 12:27:16 +0530

    feat:integrating signup page with backend

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,7 +5,7 @@ import SignIn from '../pages/SignIn'
 import SignUp1 from '../pages/SignUp1'
 import SignUp2 from '../pages/SignUp2'
 import AdminDashboard from '../pages/admin/AdminDashboard'
-
+import ProtectedRoute from '../components/ProtectedRoute'
 function Router() {
   return (
    <Routes>
@@ -13,9 +13,12 @@ function Router() {
        <Route path="/" element= {<SignIn/>} />
        <Route path="/signup" element= {<SignUp1/>} />
        <Route path="/signup2" element= {<SignUp2/>} />
-       
-       <Route path="/admin/dashboard" element={<AdminDashboard />} />
+       <Route element={<ProtectedRoute />}>
+          <Route path="/admin/dashboard" element={<AdminDashboard />}/>
+
+       </Route>
    </Routes>
+
   );
 }
 
--- src/service/authService.jsx
+++ src/service/authService.jsx
@@ -2,25 +2,30 @@
 import axios from "axios";
 
 
-const API_URL = "http://localhost:8080/api/auth/login"; 
+const BASE_URL = "http://localhost:8080/api";
 
-const login = async (email, password) => {
-    try {
-        const response = await axios.post(API_URL, { email, password });
-
-        if (response.data?.response?.token) {
-            localStorage.setItem("token", response.data.response.token); // Save token
-            return response.data;
-        } else {
-            throw new Error("Login failed, no token received.");
+const authService = {
+    signup: async (userData) =>{
+        return await axios.post(`${BASE_URL}/users/register`,userData)
+    },
+    login: async (email,password) =>{
+        try{
+            const response = await axios.post(`${BASE_URL}/auth/login`, { email, password });
+            if (response.data?.token) {
+                localStorage.setItem("token", response.data.token); // Store token
+                return response.data;
+            }
+            else {
+                throw new Error("Login failed, no token received.");
+            }
         }
-    } catch (error) {
-        throw error.response?.data?.message || "Login failed. Please check your credentials.";
+        catch (error) {
+            throw error.response?.data?.message || "Login failed. Please check your credentials.";
+        }
+    },
+    logout: () => {
+        localStorage.removeItem("token");
     }
 };
 
-const logout = () => {
-    localStorage.removeItem("token");
-};
-
-export default { login, logout };
+export default authService;
