9a1bb573af27670cccf046ad3dfe1f4102ba195c
v-sreevidya <sreevidyavijayan14@gmail.com>
2025-04-07 17:17:12 +0530

    merging

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -3,16 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp1 from "../pages/SignUp1";
 import SignUp2 from "../pages/SignUp2";
-import ForgotPassword from "../pages/ForgotPassword";
-
 import ProtectedRoute from "../components/ProtectedRoute";
-
 import RequestApproval from "../pages/admin/RequestApproval";
 import AdminViewStudent from "../pages/admin/AdminViewStudent";
 import AdminViewMentor from "../pages/admin/AdminViewMentor";
 import MentorViewStudent from "../pages/mentor/MentorViewStudent";
 import ConfirmationModal from "../components/modals/ConfirmationModal";
 import AcceptApproval from "../pages/admin/AcceptApproval";
+import ForgotPassword from "../pages/ForgotPassword";
 
 import RoleBasedDashboard, {
   RoleBasedProfile,
@@ -36,7 +34,6 @@ function Router() {
         <Route path="/admin-student-profile" element={<AdminViewStudent />} />
         <Route path="/admin-mentor-profile" element={<AdminViewMentor />} />
         <Route path="/admin-accept/:userId" element={<AcceptApproval />} />
-
 
         {/* Mentor */}
 
